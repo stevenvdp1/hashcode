5f0b906b2d29598fb5f71ece2db0f22966c01fb7
Unknown <stevenvandepoel@gmail.com>
2018-03-08 17:31:36 +0100

    48mil

--- car2.py
+++ car2.py
@@ -1,92 +1,70 @@
 class Ride:
-<<<<<<< Updated upstream
-  def __init__(self,r_id, x_s,y_s,x_e,y_e, t_s, t_e):
-    self.ride_id = int(r_id)
-    self.x_start = int(x_s)
-    self.y_start = int(y_s)
-    self.x_end = int(x_e)
-    self.y_end = int(y_e)
-    self.start_t = int(t_s)
-    self.end_t = int(t_e)
-    self.ride_length = abs(self.x_start-self.x_end)+abs(self.y_start-self.y_end)
+  def __init__(self,i,xs,ys,xe,ye,ts,te):
+      self.ride_id = int(i)
+      self.x_start = int(xs)
+      self.y_start = int(ys)
+      self.x_end = int(xe)
+      self.y_end = int(ye)
+      self.start_t = int(ts)
+      self.end_t = int(te)
+      self.length = abs(self.x_start-self.x_end)+abs(self.y_start-self.y_end)
+      self.available = True
 
 class Car:
-  def distanceToStart(self, ride):
+  def findRideWithBonus(self, rides, T):
+    bestRideTime = T
+    bestRideID = None
+    for r in range(len(rides)):
+      ride = rides[r]
+      if(ride.available):
+        if(self.timeSpent + self.lengtToStart(ride)<=ride.start_t):
+          rideTime = ride.start_t+ride.length
+        # else:
+        #   rideTime = self.timeSpent + self.lengtToStart(ride)+ride.length
+          if(rideTime<=ride.end_t and rideTime<bestRideTime):
+            bestRideID = ride.ride_id
+            bestRideTime = rideTime
+    if(bestRideID != None):
+      self.x = rides[bestRideID].x_end
+      self.y = rides[bestRideID].y_end
+      self.timeSpent = bestRideTime
+    return bestRideID
+
+  def findRideWithoutBonus(self, rides, T):
+    bestRideTime = T
+    bestRideID = None
+    for r in range(len(rides)):
+      ride = rides[r]
+      if(ride.available):
+        if(self.timeSpent + self.lengtToStart(ride)<=ride.start_t):
+          rideTime = ride.start_t+ride.length
+        else:
+          rideTime = self.timeSpent + self.lengtToStart(ride)+ride.length
+        if(rideTime<=ride.end_t and rideTime<bestRideTime):
+          bestRideID = ride.ride_id
+          bestRideTime = rideTime
+    if(bestRideID != None):
+      self.x = rides[bestRideID].x_end
+      self.y = rides[bestRideID].y_end
+      self.timeSpent = bestRideTime
+    return bestRideID
+
+  def lengtToStart(self, ride):
     return abs(self.x-ride.x_start)+abs(self.y-ride.y_start)
-  
-  def findRide(self, rides):
-    pass
 
-  def __init__(self,x,y):
-        self.x = int(x)
-        self.y = int(y)
-        self.rideIdArray = []
-        self.distance_travelled = 0
+  def __init__(self, a, b):
+      self.x = int(a)
+      self.y = int(b)
+      self.timeSpent = 0
+      self.completedRides = []
+      self.isFinished = False
 
 
-
-
-f = open("input/a_example.in", "r")
+# f = open("input/a_example.in", "r")
 # f = open("input/b_should_be_easy.in", "r")
 # f = open("input/c_no_hurry.in", "r")
-# f = open("input/d_metropolis.in", "r")
+f = open("input/d_metropolis.in", "r")
 # f = open("input/e_high_bonus.in", "r")
-
-g = f.readline().split()
-
-row = int(g[0])
-col = int(g[1])
-veh = int(g[2])
-rid = int(g[3])
-bon = int(g[4])
-ste = int(g[5])
-
-def initRides():
-  rides = []
-  for x in range(rid):
-    ride = f.readline().split()
-    rides.append(Ride(x, ride[0],ride[1],ride[2],ride[3],ride[4],ride[5]))
-  return rides
-
-def initCars():
-  cars = []
-  for x in range(veh):
-    cars.append(Car(0,0))
-  return cars
-
-def main():
-  rides = initRides()
-  cars = initCars()
-
-
-if __name__ == "__main__":
-  main()
-=======
-
-    def __init__(self,i,xs,ys,xe,ye,ts,te):
-        self.ride_id = int(i)
-        self.x_start = int(xs)
-        self.y_start = int(ys)
-        self.x_end = int(xe)
-        self.y_end = int(ye)
-        self.start_t = int(ts)
-        self.end_t = int(te)
-        self.length = abs(self.x_start-self.x_end)+abs(self.y_start-self.y_end)
-
-class Car:
-    completedRides = []
-    
-    def __init__(self, a, b):
-        self.x = int(a)
-        self.y = int(b)
-
-
-
-f = open("a_example.in", "r")
-# f = open("b_should_be_easy.in", "r")
-# f = open("c_no_hurry.in", "r")
-# f = open("d_metropolis.in", "r")
-# f = open("e_high_bonus.in", "r")
 
 g = f.readline().split()
 
@@ -101,10 +79,48 @@ cars = []
 rides = []
 
 for x in range(F):
-    cars.append(Car(0,0))
+  cars.append(Car(0,0))
 
 for x in range(N):
-    ride = f.readline().split()
-    rides.append(Ride(x, ride[0],ride[1],ride[2],ride[3],ride[4],ride[5])) 
+  ride = f.readline().split()
+  rides.append(Ride(x, ride[0],ride[1],ride[2],ride[3],ride[4],ride[5]))
 
->>>>>>> Stashed changes
+for c in range(F):
+  print(c)
+  bestRide = cars[c].findRideWithBonus(rides, T)
+  if(bestRide != None):
+    rides[bestRide].available = False
+    cars[c].completedRides.append(bestRide)
+
+for c in range(F):
+  print(c)
+  while(True):
+    bestRide = cars[c].findRideWithBonus(rides, T)
+    if(bestRide != None):
+      rides[bestRide].available = False
+      cars[c].completedRides.append(bestRide)
+    else:
+      break
+
+for c in range(F):
+  print(c)
+  while(True):
+    bestRide = cars[c].findRideWithoutBonus(rides, T)
+    if(bestRide != None):
+      rides[bestRide].available = False
+      cars[c].completedRides.append(bestRide)
+    else:
+      break
+
+# outp = open("output/output_a.in", "w")
+# outp = open("output/output_b.in", "w")
+# outp = open("output/output_c.in", "w")
+outp = open("output/output_d.in", "w")
+# outp = open("output/output_e.in", "w")
+
+for c in range(F):
+    outp.write(str(len(cars[c].completedRides)))
+    for r in range(len(cars[c].completedRides)):
+        outp.write(" "+str(cars[c].completedRides[r]))
+    outp.write("\n")
+outp.close()
